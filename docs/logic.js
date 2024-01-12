9bd68e43433c871730252f681065fff4ba2eb4ab
Ines <link_ines@gmx.de>
2024-01-12 11:51:45 +0100

    move target on click

--- docs/logic.js
+++ docs/logic.js
@@ -1,11 +1,21 @@
-document.addEventListener("DOMContentLoaded",(event)=>{ 
-    console.log("Hello World")
-    const e_target = document.getElementById("target")
-    const e_playarea = document.getElementById("playarea")
-    e_playarea.addEventListener("click", (event) =>{
-        console.log(event.target === e_target ? "Treffer":"Daneben")
-    })
+document.addEventListener("DOMContentLoaded", (event) => {
+    const e_target = document.getElementById("target");
+    const e_playarea = document.getElementById("playarea");
+    e_playarea.addEventListener("click", (event) => {
+        console.log(event.target === e_target ? "Treffer" : "Daneben");
+        const target = {
+            width: 200,
+            height: 150,
+        };
 
-})
-    
-    
+        const playarea = {
+            width: 800,
+            height: 600,
+        };
+
+        const top = Math.random() * (playarea.height - target.height);
+        const left = Math.random() * (playarea.width - target.width);
+        e_target.style.top = `${top}px`;
+        e_target.style.left = `${left}px`;
+    });
+});
